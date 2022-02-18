8c9df60baa12dbb0de43e1dedb84b2eb3f1bdcad
Matota <72810000+mat0ta@users.noreply.github.com>
2022-02-18 19:14:30 +0100

    Realizado ejercicio 9

---
+++ ejercicios/ejercicio9.py
@@ -0,0 +1,9 @@
+# 1.)
+numeros = [280, 15, 78]
+media = (numeros[0] + numeros[1] + numeros[2]) / 3
+print(media)
+
+# 2.)
+ponderacion = [0.4, 0.5, 0.1]
+media_ponderada = (numeros[0] * ponderacion[0] + numeros[1] * ponderacion[1] + numeros[2] * ponderacion[2]) / 3
+print(media_ponderada)
