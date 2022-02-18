7ec100923ad4e1c9200b2b4df64d97dec114c153
Matota <72810000+mat0ta@users.noreply.github.com>
2022-02-18 19:21:30 +0100

    Realizado ejercicio 10

---
+++ ejercicios/ejercicio10.py
@@ -0,0 +1,11 @@
+# 1.)
+lado_1 = 20
+altura = 40
+area = (lado_1 * altura) / 2
+print(area)
+
+# 2.)
+lado_1_rectangulo = 20
+lado_2_rectangulo = 60
+area_rectangulo = (lado_1_rectangulo * lado_2_rectangulo) / 2
+print(area_rectangulo)
